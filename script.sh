07720ca291f9f866d8558ce91f094bae772100f5
Deobrat QMatX <137123400+deobratsingh9@users.noreply.github.com>
2026-06-08 10:46:14 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #SBATCH -c 2
 #SBATCH -t 001:00:00
 #SBATCH -J Si-optical
-#SBATCH -A naiss2025-22-1789
+#SBATCH -A xxxxx
 
 module load QuantumESPRESSO/7.2-nsc1-intel-2018b-eb
 export OMP_NUM_THREADS=2
